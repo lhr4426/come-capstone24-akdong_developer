c20f8c0a214489434e4aa6275318a25b11b9abd0
changin99 <20217140@edu.hanbat.ac.kr>
2024-05-06 18:00:43 +0900

    edit datatype

--- 003 Code/asset_http_go/models/asset_model.go
+++ 003 Code/asset_http_go/models/asset_model.go
@@ -10,8 +10,8 @@ type Asset struct {
 	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Name          string             `bson:"name" json:"name" validate:"required"`
 	CategoryID    int                `bson:"categoryid" json:"categoryid" validate:"required"`
-	Thumbnail     string             `bson:"thumbnail" json:"thumbnail" validate:"required"` // 썸네일의 URL
-	File          int                `bson:"file" json:"file" validate:"required"`           // 파일 DB의 PK
+	Thumbnail     []byte             `bson:"thumbnail" json:"thumbnail" validate:"required"` // 썸네일의
+	File          []byte             `bson:"file" json:"file" validate:"required"`           // 파일 DB의 PK
 	UploadDate    time.Time          `bson:"uploaddate" json:"uploaddate"`
 	DownloadCount int                `bson:"downloadcount" json:"downloadcount"`
 	Price         float64            `bson:"price" json:"price"`
