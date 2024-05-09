c55cf96d254974556788a3d50040914dc8778c81
yj <20211939@edu.hanbat.ac.kr>
2024-05-09 16:05:55 +0900

    edit

--- 003 Code/map_http_go/controllers/map_controllers.go
+++ 003 Code/map_http_go/controllers/map_controllers.go
@@ -28,34 +28,36 @@ func CreateMap() gin.HandlerFunc {
 		mapdata := make(map[string]interface{}) // 빈 맵 생성(동적으로 key-value쌍 저장 가능)
 		// var map_data models.MapData // map 함수 이름 존재하므로 map쓰면 안됨 + 동적 저장이므로 struct 필요 없음
 
-		// 예외처리(기존에 있는 정보인지 확인 필요, 기존에 있는 정보라면 변경) // 검증
-
 		//body 유효성 검증
 		if err := c.BindJSON(&mapdata); err != nil {
-			c.JSON(http.StatusBadRequest, responses.MapResponse{Code: 0, Message: "error"})
+			c.JSON(http.StatusBadRequest, responses.MapResponse{Code: 0, Message: "body error"})
 			return
 		}
+
+		fmt.Println("2222222222222", mapdata) // 어떤거 나오는지 비교, json 받아온 값 확인
 
 		// mapdata에서 map_id, version, chunkNum 확인
 		mapId := mapdata["map_id"].(float64)
 		mapVersion := mapdata["version"].(float64)
 		mapChunk := mapdata["chunkNum"].(float64)
 
+		fmt.Println("3333333333333", mapId, mapVersion, mapChunk) 
+
+		// 중복 확인을 위한 filter
 		filter := bson.M{
 			"map_id":   mapId,
 			"version":  mapVersion,
 			"chunkNum": mapChunk,
 		}
 
-		var checkData map[string]interface{}
-		err := mapCollection.FindOne(ctx, filter).Decode(&checkData)
+		err := mapCollection.FindOne(ctx, filter).Decode(&mapdata)
 
-		// Insert
+		// 중복 없을 때 Insert
 		if err != nil {
 			if err == mongo.ErrNoDocuments {
 				_, err := mapCollection.InsertOne(ctx, mapdata) // DB에 바로 저장
 				if err != nil {
-					c.JSON(http.StatusInternalServerError, responses.MapResponse{Code: 0, Message: "error"})
+					c.JSON(http.StatusInternalServerError, responses.MapResponse{Code: 0, Message: "insert error"})
 					return
 				}
 				c.JSON(http.StatusCreated, responses.MapResponse{Code: 1, Message: "insert success"})
@@ -65,12 +67,23 @@ func CreateMap() gin.HandlerFunc {
 			return
 		}
 
-		// Update
+		// Update(모두 삭제 하고 Insert)
+		// 모두 삭제(if chunkNum이 더 있으면)
 		_, err = mapCollection.ReplaceOne(ctx, filter, mapdata)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, responses.MapResponse{Code: 0, Message: "error"})
 			return
 		}
+
+		// 만약 map_id, version, chunk(개수), mapdata 제외하고 map_id, version있는거 모두 삭제
+			// 만약 짧은 정보가 Update 된다면 나머지 Chunk_Num을 삭제
+			// 첫번째가 중복되면 이후 chunk_num 모두 삭제하기, 그러면 다음부터는 create가능
+
+		chk_filter := bson.M{
+			"chunkNum": mapChunk
+		}
+
+		chk_err := mapCollection.DeleteMany(ctx, ).Decode(&mapdata)
 
 		c.JSON(http.StatusOK, responses.MapResponse{Code: 1, Message: "update success"})
 
@@ -125,6 +138,6 @@ func GetMap() gin.HandlerFunc {
 		}
 
 		// 데이터 반환
-		c.JSON(http.StatusOK, responses.MapResponse_map{Code: 1, Message: filtermapinfo})
+		c.JSON(http.StatusOK, responses.MapResponse_map{Code: 1, Message: filtermapinfo}) // 형변환
 	}
 }
