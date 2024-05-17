a79100bdc5453ce825395b571ab717b74bfd3f52
yj <20211939@edu.hanbat.ac.kr>
2024-05-17 21:17:59 +0900

    editdocker

--- 003 Code/map_http_go/configs/setup.go
+++ 003 Code/map_http_go/configs/setup.go
@@ -12,7 +12,7 @@ import (
 func ConnectDB() *mongo.Client {
 
 	// 접속할 MongoDB 주소 설정
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017")
 
 	// MongoDB 연결
 	client, err := mongo.Connect(context.TODO(), clientOptions)
